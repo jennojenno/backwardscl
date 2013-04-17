f9699656c45369d9f16a366d722000b5f635894c
Jen Lim <jennolim@gmail.com>
2013-04-17 16:53:56 -0400

    New rails project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backwardscl::Application.config.secret_token = '53e2705357bcfe4e8de3d29bd3dcb1f3114d1f1b85f2ee6b41c9b6e407eb3be490fa8bb7c254f87dafb35cf88f90767f5b0fc724a465f23e95a47ba117aa14b8'
